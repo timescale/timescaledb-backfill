31af0a0f9a213d03daac9444e8086809a19cfa45
James Guthrie <jguthrie@timescale.com>
2023-07-11 17:06:18 +0200

    chore: refactor

--- src/execute.rs
+++ src/execute.rs
@@ -14,8 +14,8 @@ pub async fn copy_chunk(source: &mut Source, target: &mut Target, chunk: Chunk) 
     let target_tx = target.client.transaction().await?;
 
     let compressed_chunk = get_compressed_chunk(&source_tx, &chunk).await?;
-    if compressed_chunk.is_none() {
-        //create_compressed_chunk(target, &chunk).await?;
+    if compressed_chunk.is_some() {
+        // TODO: create compressed chunk in target, if missing
     }
     copy_uncompressed_chunk_data(&source_tx, &target_tx, &chunk).await?;
     if compressed_chunk.is_some() {
@@ -25,79 +25,6 @@ pub async fn copy_chunk(source: &mut Source, target: &mut Target, chunk: Chunk) 
     target_tx.commit().await?;
     source_tx.commit().await?;
     Ok(())
-}
-
-struct CompressedChunk {
-    chunk_schema: String,
-    chunk_name: String,
-}
-
-async fn get_compressed_chunk(
-    source_tx: &Transaction<'_>,
-    chunk: &Chunk,
-) -> Result<Option<CompressedChunk>> {
-    let row = source_tx
-        .query_opt(
-            r#"
-    SELECT
-      cch.schema_name
-    , cch.table_name
-    FROM _timescaledb_catalog.chunk ch
-    JOIN _timescaledb_catalog.chunk cch ON ch.compressed_chunk_id = cch.id
-    WHERE ch.schema_name = $1
-      AND ch.table_name = $2
-    "#,
-            &[&chunk.chunk_schema, &chunk.chunk_name],
-        )
-        .await?;
-    Ok(row.map(|r| CompressedChunk {
-        chunk_schema: r.get("schema_name"),
-        chunk_name: r.get("table_name"),
-    }))
-}
-
-async fn get_chunk_with_same_dimensions(
-    tx: &Transaction<'_>,
-    chunk: &Chunk,
-) -> Result<Option<Chunk>> {
-    let row = tx
-        .query_opt(
-            r#"
-    SELECT
-          h.schema_name as hypertable_schema
-        , h.table_name as hypertable_name
-        , ch.schema_name as chunk_schema
-        , ch.table_name as chunk_name
-        , ds.range_start as dimension_start
-        , ds.range_end as dimension_end
-    FROM
-         _timescaledb_catalog.chunk ch
-    JOIN _timescaledb_catalog.chunk_constraint cons ON cons.chunk_id = ch.id
-    JOIN _timescaledb_catalog.dimension_slice ds ON ds.id = cons.dimension_slice_id
-    JOIN _timescaledb_catalog.hypertable h ON h.id = ch.hypertable_id
-    WHERE h.schema_name = $1
-      AND h.table_name = $2
-      AND ds.range_start = $3
-      AND ds.range_end = $4
-      ;
-    "#,
-            &[
-                &chunk.hypertable_schema,
-                &chunk.hypertable_name,
-                &chunk.dimension_start,
-                &chunk.dimension_end,
-            ],
-        )
-        .await?;
-    Ok(row.map(|r| Chunk {
-        hypertable_schema: r.get("hypertable_schema"),
-        hypertable_name: r.get("hypertable_name"),
-        chunk_schema: r.get("chunk_schema"),
-        chunk_name: r.get("chunk_name"),
-        dimension_start: r.get("dimension_start"),
-        dimension_end: r.get("dimension_end"),
-        active_chunk: false,
-    }))
 }
 
 async fn copy_uncompressed_chunk_data(
@@ -121,11 +48,13 @@ async fn copy_uncompressed_chunk_data(
         quote_ident(&target_chunk.chunk_name)
     );
 
-    truncate_chunk(target_tx, &target_chunk_name).await?;
+    if !chunk.active_chunk {
+        truncate_chunk(target_tx, &target_chunk_name).await?;
+    } else {
+        // TODO: delete rows in active chunk where `time` < completion point
+    }
     copy_chunk_from_source_to_target(source_tx, target_tx, &source_chunk_name, &target_chunk_name)
         .await
-
-    // TODO: handle current chunk is active chunk
 }
 
 async fn truncate_chunk(target_tx: &Transaction<'_>, chunk_name: &str) -> Result<()> {
@@ -212,4 +141,77 @@ pub fn quote_ident(value: &str) -> String {
     }
     result.push('"');
     result
+}
+
+struct CompressedChunk {
+    chunk_schema: String,
+    chunk_name: String,
+}
+
+async fn get_compressed_chunk(
+    source_tx: &Transaction<'_>,
+    chunk: &Chunk,
+) -> Result<Option<CompressedChunk>> {
+    let row = source_tx
+        .query_opt(
+            r#"
+    SELECT
+      cch.schema_name
+    , cch.table_name
+    FROM _timescaledb_catalog.chunk ch
+    JOIN _timescaledb_catalog.chunk cch ON ch.compressed_chunk_id = cch.id
+    WHERE ch.schema_name = $1
+      AND ch.table_name = $2
+    "#,
+            &[&chunk.chunk_schema, &chunk.chunk_name],
+        )
+        .await?;
+    Ok(row.map(|r| CompressedChunk {
+        chunk_schema: r.get("schema_name"),
+        chunk_name: r.get("table_name"),
+    }))
+}
+
+async fn get_chunk_with_same_dimensions(
+    tx: &Transaction<'_>,
+    chunk: &Chunk,
+) -> Result<Option<Chunk>> {
+    let row = tx
+        .query_opt(
+            r#"
+    SELECT
+          h.schema_name as hypertable_schema
+        , h.table_name as hypertable_name
+        , ch.schema_name as chunk_schema
+        , ch.table_name as chunk_name
+        , ds.range_start as dimension_start
+        , ds.range_end as dimension_end
+    FROM
+         _timescaledb_catalog.chunk ch
+    JOIN _timescaledb_catalog.chunk_constraint cons ON cons.chunk_id = ch.id
+    JOIN _timescaledb_catalog.dimension_slice ds ON ds.id = cons.dimension_slice_id
+    JOIN _timescaledb_catalog.hypertable h ON h.id = ch.hypertable_id
+    WHERE h.schema_name = $1
+      AND h.table_name = $2
+      AND ds.range_start = $3
+      AND ds.range_end = $4
+      ;
+    "#,
+            &[
+                &chunk.hypertable_schema,
+                &chunk.hypertable_name,
+                &chunk.dimension_start,
+                &chunk.dimension_end,
+            ],
+        )
+        .await?;
+    Ok(row.map(|r| Chunk {
+        hypertable_schema: r.get("hypertable_schema"),
+        hypertable_name: r.get("hypertable_name"),
+        chunk_schema: r.get("chunk_schema"),
+        chunk_name: r.get("chunk_name"),
+        dimension_start: r.get("dimension_start"),
+        dimension_end: r.get("dimension_end"),
+        active_chunk: false,
+    }))
 }
