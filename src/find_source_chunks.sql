d81d86b0f3517272d2875e958bc7d911f782f5d1
Alejandro Do Nascimento Mora <alejandrodnm@gmail.com>
2023-07-21 20:51:59 +0200

    feat: pull tasks from the queue

--- src/find_source_chunks.sql
+++ src/find_source_chunks.sql
@@ -1,20 +1,19 @@
+-- TODO: do we need to migrate the HT and then the CAGG? We are already
+-- dropping the triggers so it might not make a difference.
 /*
 $1 is a case insensitive posix regular expression filtering on hypertable schema.table
 $2 is an optional text value representing an upper bound on "time" dimension values
 */
 select
-  c.id as chunk_id
-, c.schema_name as chunk_schema
+  c.schema_name as chunk_schema
 , c.table_name as chunk_name
-, h.id as hypertable_id
 , h.schema_name as hypertable_schema
 , h.table_name as hypertable_name
 , (
     select json_agg
     (
         json_build_object
-        ( 'dimension_id', d2.id
-        , 'column_name', d2.column_name
+        ( 'column_name', d2.column_name
         , 'column_type', d2.column_type
         , 'range_start', ds2.range_start
         , 'range_end', ds2.range_end
@@ -24,9 +23,9 @@ select
     from _timescaledb_catalog.chunk_constraint cc2
     inner join _timescaledb_catalog.dimension_slice ds2 on (cc2.chunk_id = c.id and cc2.dimension_slice_id = ds2.id)
     inner join _timescaledb_catalog.dimension d2 on (ds2.dimension_id = d2.id and d2.hypertable_id = h.id)
-  ) as dimensions
+  )::TEXT as dimensions
 , case when $2::text is not null and ds.range_start <= d.filter_value and d.filter_value < ds.range_end
-    then format('where %I <= %s', d.column_name, d.filter_literal)
+    then format('%I <= %s', d.column_name, d.filter_literal)
   end as filter
 from
 (
@@ -49,20 +48,24 @@ inner join lateral
             _timescaledb_internal.time_to_internal($2::text::timestamptz)
         when d.column_type = 'timestamp'::regtype then
             _timescaledb_internal.time_to_internal($2::text::timestamp)
+        when d.column_type = 'timestamptz'::regtype then
+            _timescaledb_internal.time_to_internal($2::text::timestamptz)
         when d.column_type = 'date'::regtype then
             _timescaledb_internal.time_to_internal($2::text::date)
-        when d.column_type in ('bigint'::regtype, 'int'::regtype, 'smallint'::regtype) then
-            $2::text::bigint
+        when d.column_type in ('bigint'::regtype, 'int'::regtype, 'smallint'::regtype) and regexp_like($2::text, '^[0-9]+$') then
+            _timescaledb_internal.time_to_internal(cast($2::text as bigint))
       end as filter_value
     , case
         when d.column_type = 'tstzrange'::regtype then
             format('%L', $2::text::timestamptz)
         when d.column_type = 'timestamp'::regtype then
             format('%L', $2::text::timestamp)
+        when d.column_type = 'timestamptz'::regtype then
+            format('%L', $2::text::timestamptz)
         when d.column_type = 'date'::regtype then
             format('%L', $2::text::date)
-        when d.column_type in ('bigint'::regtype, 'int'::regtype, 'smallint'::regtype) then
-            format('%L', $2::text::bigint)
+        when d.column_type in ('bigint'::regtype, 'int'::regtype, 'smallint'::regtype) and regexp_like($2::text, '^[0-9]+$') then
+            format('%L', cast($2::text as bigint))
       end as filter_literal
     from _timescaledb_catalog.dimension d
     where d.hypertable_id = h.id
--- src/workers.rs
+++ src/workers.rs
@@ -1,12 +1,10 @@
 use crate::connect::{Source, Target};
 use crate::execute::copy_chunk;
-use crate::timescale::Chunk;
+use crate::task::{claim_copy_task, complete_copy_task};
 use anyhow::{Context, Result};
-use async_channel::Receiver;
-use chrono::{DateTime, Utc};
 use tokio::task::JoinSet;
 use tokio_postgres::Config;
-use tracing::{debug, trace};
+use tracing::trace;
 
 /// Spawns asyncronous tasks (workers) that wait until a `Chunk` is
 /// passed via a `Receiver`, and copy the data of the `Chunk` from the source
@@ -14,8 +12,6 @@ use tracing::{debug, trace};
 pub struct Pool {
     num_workers: usize,
     tasks: JoinSet<Result<()>>,
-    rx: Receiver<Chunk>,
-    until: DateTime<Utc>,
     source_config: Config,
     target_config: Config,
 }
@@ -24,18 +20,10 @@ impl Pool {
     /// Create a new worker pool. Workers will each open a connection to
     /// both source and target with the given `Config`s, and processes the
     /// `Chunk`s received on the channel.
-    pub async fn new(
-        num_workers: usize,
-        rx: Receiver<Chunk>,
-        until: DateTime<Utc>,
-        source_config: &Config,
-        target_config: &Config,
-    ) -> Self {
+    pub async fn new(num_workers: usize, source_config: &Config, target_config: &Config) -> Self {
         let mut pool = Self {
             num_workers,
             tasks: JoinSet::new(),
-            rx: rx.clone(),
-            until,
             source_config: source_config.clone(),
             target_config: target_config.clone(),
         };
@@ -59,30 +47,34 @@ impl Pool {
     }
 
     async fn add_worker(&mut self) {
-        let rx_clone = self.rx.clone();
         let source_config_clone = self.source_config.clone();
         let target_config_clone = self.target_config.clone();
-        let until = self.until;
-        self.tasks.spawn(async move {
-            worker_run(&rx_clone, &source_config_clone, &target_config_clone, until).await
-        });
+        self.tasks
+            .spawn(async move { worker_run(&source_config_clone, &target_config_clone).await });
     }
 }
 
 /// Execution loop of a worker. It receives chunks from a channel and executes
 /// the `copy_chunk` function.
-async fn worker_run(
-    rx: &Receiver<Chunk>,
-    source_config: &Config,
-    target_config: &Config,
-    until: DateTime<Utc>,
-) -> Result<()> {
+async fn worker_run(source_config: &Config, target_config: &Config) -> Result<()> {
     let mut source = Source::connect(source_config).await?;
     let mut target = Target::connect(target_config).await?;
 
-    while let Ok(chunk) = rx.recv().await {
-        debug!("copying chunk: {chunk:?}");
-        copy_chunk(&mut source, &mut target, chunk, &until).await?;
+    loop {
+        let target_tx = target.client.transaction().await?;
+        match claim_copy_task(&target_tx)
+            .await
+            .with_context(|| "error claiming copy task")?
+        {
+            Some(copy_task) => {
+                let source_tx = source.transaction().await?;
+                copy_chunk(&source_tx, &target_tx, &copy_task).await?;
+                complete_copy_task(&target_tx, &copy_task).await?;
+                source_tx.commit().await?;
+            }
+            None => break,
+        }
+        target_tx.commit().await?;
     }
     Ok(())
 }
